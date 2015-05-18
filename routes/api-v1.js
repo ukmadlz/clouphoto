11d456a9baa0f902c3af7e0b9702a75b4e83193f
Mike Elsmore <mike@elsmore.me>
2015-05-18 18:59:36 +0100

    A bit of twilio, a bit of black magic and some bower…

--- routes/api-v1.js
+++ routes/api-v1.js
@@ -1,8 +1,32 @@
-var express = require('express');
-var router = express.Router();
+var express  = require('express');
+var router   = express.Router();
+var config   = require('../config');
+var cloudant = require('cloudant')(config.cloudant);
+var db       = cloudant.use('clouphoto');
+var fs       = require('fs');
+var request  = require('request');
 
-/* GET home page. */
-router.get('/message', function(req, res, next) {
+/* POST save message. */
+router.post('/message', function(req, res, next) {
+  db.insert(req.body, function(err, body) {
+    console.log(req.body);
+    request.get(req.body.MediaUrl0, function(error, response, data) {
+      if (!error && response.statusCode == 200) {
+        // Continue with your processing here.
+        var fileExtension =  req.body.MediaContentType0.split('/')[1];
+        var fileName = body.id + '.' + fileExtension;
+        db.attachment.insert(body.id, fileName, data, req.body.MediaContentType0,
+          { rev: body.rev }, function(err, body) {
+            if (!err)
+              console.log(body)
+            else
+              console.log('ERROR', err);
+          });
+      }
+    });
+
+  });
+
   res.json({error:'0'})
 });
 
